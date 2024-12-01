7e68893dd55be5b6285d0e5198343be2369b7987
Konstantin <goduskospan@gmail.com>
2024-12-01 23:01:01 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import './App.css';
 import demoImg from './demo.jpg';
 import {useEffect, useRef} from "react";
-import {Button, ButtonGroup, Card, Col, Container, Row} from "react-bootstrap";
+import {
+    Button,
+    ButtonGroup,
+    Card,
+    Col,
+    Container,
+    Dropdown,
+    DropdownButton,
+    Form,
+    FormGroup,
+    Row
+} from "react-bootstrap";
 
 const tg = window.Telegram.WebApp;
 
@@ -42,7 +53,7 @@ function setDemoImage() {
     };
     image.src = demoImg;
     elements.image = image;
-    // getDrawCursor()
+    getDrawCursor()
 }
 
 function handleCanvasForImage(image) {
@@ -63,12 +74,169 @@ function handleCanvasForImage(image) {
     canvases.preview.ctx.drawImage(image, 0, 0, options.width, options.height);
 }
 
+function getDrawCursor(brushSize = options.brush.size) {
+    const circle = `
+		<svg
+			height="${brushSize}"
+			width="${brushSize}"
+			fill="rgb(0 0 0 / 25%)"
+			viewBox="0 0 ${brushSize * 2} ${brushSize * 2}"
+			xmlns="http://www.w3.org/2000/svg"
+		>
+			<circle
+				cx="50%"
+				cy="50%"
+				r="${brushSize}" 
+				stroke="#e4e4e4"
+                stroke-width="1"
+			/>
+		</svg>
+	`;
+    const cursor = `data:image/svg+xml;base64,${window.btoa(circle)}`;
+    canvases.drawing.canvas.style.cursor = `url("${cursor}") ${brushSize / 2} ${brushSize / 2}, crosshair`
+}
+
+function setMode(mode) {
+    options.mode = mode
+}
+
+function bootstrapRangeEvents() {
+    const ranges = [
+        {
+            id: "brush-opacity",
+            handler: (el, textEl) => {
+                textEl.innerText = el.value;
+                options.brush.opacity = (parseInt(el.value) / 100).toFixed(2);
+            },
+        },
+        {
+            id: "brush-size",
+            handler: (el, textEl) => {
+                textEl.innerText = el.value;
+                options.brush.size = parseInt(el.value);
+            },
+        },
+    ];
+
+    ranges.forEach(range => {
+        const el = document.getElementById(range.id);
+        const textEl = document.getElementById(`${range.id}--value`);
+        el.addEventListener("change", (e) => {
+            range.handler(el, textEl)
+            getDrawCursor();
+        })
+    })
+}
+
+function bootstrapCanvasEvents() {
+    canvases.drawing.canvas.addEventListener("mousemove", draw);
+    canvases.drawing.canvas.addEventListener("click", (e) => {
+        options.isDrawing = true;
+        draw(e)
+        options.isDrawing = false;
+    });
+    canvases.drawing.canvas.addEventListener("mousedown", () => {
+        options.isDrawing = true;
+    });
+    canvases.drawing.canvas.addEventListener("mouseup", () => {
+        options.isDrawing = false;
+    });
+    canvases.drawing.canvas.addEventListener("mouseout", () => {
+        options.isDrawing = false;
+    });
+}
+
+function draw(e) {
+    if (!options.isDrawing) return;
+
+    const x = e.offsetX;
+    const y = e.offsetY;
+
+    if (options.mode === "draw") {
+        canvases.drawing.ctx.fillStyle = canvases.drawing.ctx.shadowColor = `rgba(0,0,0,${options.brush.opacity})`;
+    } else {
+        canvases.drawing.ctx.fillStyle = canvases.drawing.ctx.shadowColor = `rgba(255,255,255,${options.brush.opacity})`;
+    }
+
+    canvases.drawing.ctx.lineJoin = canvases.drawing.ctx.lineCap = 'round';
+    canvases.drawing.ctx.shadowBlur = options.brush.hardness;
+    canvases.drawing.ctx.beginPath();
+    canvases.drawing.ctx.arc(x, y, options.brush.size / 2, 0, Math.PI * 2);
+    canvases.drawing.ctx.fill();
+
+    requestAnimationFrame(applyMask);
+}
+
+function applyMask() {
+    // Get the image data for the drawing canvas.
+    const idata = canvases.drawing.ctx.getImageData(0, 0, options.width, options.height);
+
+    // Create a 32-bit array from the image data buffer.
+    const data32 = new Uint32Array(idata.data.buffer);
+
+    // Apply a left shift of 8 bits to each 32-bit value in the array.
+    let i = 0, len = data32.length;
+    while (i < len) data32[i] = data32[i++] << 8;
+
+    // Update the preview canvas with the masked image data.
+    canvases.preview.ctx.putImageData(idata, 0, 0);
+
+    // Set the global composite operation to "source-in".
+    canvases.preview.ctx.globalCompositeOperation = "source-in";
+
+    // Draw the image on the preview canvas with the applied mask.
+    canvases.preview.ctx.drawImage(elements.image, 0, 0);
+}
+
+function downloadFile(type) {
+    let canvas = canvases.preview.canvas;
+    let name = "image.png"
+    if (type === "mask") {
+        canvas = canvases.drawing.canvas;
+        name = "mask.png"
+    }
+    const link = document.createElement("a");
+    link.download = name;
+    link.href = canvas.toDataURL();
+    link.click();
+}
+
+function clearCanvases() {
+    canvases.drawing.ctx.clearRect(0, 0, options.width, options.height)
+    canvases.preview.ctx.clearRect(0, 0, options.width, options.height)
+}
+
+function loadImage() {
+    const file = document.getElementById('file-input').files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = function (e) {
+        const image = new Image();
+        image.onload = function () {
+            clearCanvases()
+            handleCanvasForImage(image)
+        };
+        image.src = e.target.result;
+        elements.image = image;
+    }
+    getDrawCursor();
+}
+
 function App() {
+    const chooseButton = useRef(null);
+    const fileInput = useRef(null);
+
     const drawingCanvas = useRef(null);
     const previewCanvas = useRef(null);
 
 
     useEffect(() => {
+        elements.chooseButton = chooseButton.current;
+        elements.fileInput = fileInput.current;
+
+        elements.fileInput.addEventListener("change", loadImage);
+        elements.chooseButton.addEventListener("click", () => elements.fileInput.click());
+
         const drawingCtx = drawingCanvas.current.getContext("2d", {willReadFrequently: true});
         const previewCtx = previewCanvas.current.getContext("2d");
 
@@ -82,6 +250,8 @@ function App() {
             ctx: previewCtx
         };
 
+        bootstrapCanvasEvents();
+        bootstrapRangeEvents();
         setDemoImage();
 
         tg.ready();
@@ -109,9 +279,34 @@ function App() {
                         <Card>
                             <Card.Body>
                                 <ButtonGroup>
-                                    <Button>Mask</Button>
-                                    <Button>Unmask</Button>
+                                    <Button variant="primary" onClick={() => setMode('draw')}>Mask</Button>
+                                    <Button variant="primary" onClick={() => setMode('erase')}>Unmask</Button>
                                 </ButtonGroup>
+
+                                <FormGroup>
+                                    <Form.Label>Brush Size</Form.Label>
+                                    <span>32</span>
+                                    <Form.Range/>
+                                </FormGroup>
+
+                                <FormGroup>
+                                    <Form.Label>Brush Opacity</Form.Label>
+                                    <span>32</span>
+                                    <Form.Range/>
+                                </FormGroup>
+
+                                <input type="file"
+                                       className="d-none"
+                                       ref={fileInput}
+                                       accept="image/*"
+                                       hidden/>
+
+                                <Button variant="primary" ref={chooseButton}>Choose Image</Button>
+
+                                <DropdownButton title="Dropdown" id="bg-nested-dropdown">
+                                    <Dropdown.Item onClick={() => downloadFile('image')}>Image</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => downloadFile('mask')}>Mask</Dropdown.Item>
+                                </DropdownButton>
                             </Card.Body>
                         </Card>
                     </Row>
